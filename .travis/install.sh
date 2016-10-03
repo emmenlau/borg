d60df0d22671001bf4779ea4a13520be26e6b633
Marian Beermann <public@enkore.de>
2016-10-03 17:16:40 +0200

    well then maybe like this?

--- .travis/install.sh
+++ .travis/install.sh
@@ -32,7 +32,7 @@ if [[ "$(uname -s)" == 'Darwin' ]]; then
     brew link xz
     brew link pyenv
     brew link pkg-config
-    brew link osxfuse
+    brew link fuse
 
     case "${TOXENV}" in
         py34)
